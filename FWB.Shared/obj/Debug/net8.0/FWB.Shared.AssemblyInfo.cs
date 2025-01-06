9deb7ef31abca55ad5e75abae367d7b8ac684efd
7sam.fa <59307399+HusamFarajallah@users.noreply.github.com>
2025-01-06 11:41:05 +0200

    -

--- FWB.Shared/obj/Debug/net8.0/FWB.Shared.AssemblyInfo.cs
+++ FWB.Shared/obj/Debug/net8.0/FWB.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FWB.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a5b757107bc2fcb5c04f94c1afe6f3945295f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc490d660e2aaca345414651fbb7e438b0ac55a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FWB.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FWB.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
